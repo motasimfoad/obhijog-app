24304e66a44a24eb6bc8f02e8186a2d6f5cf097e
motasimfoad <motasimfoad@gmail.com>
2019-06-19 22:27:59 +0600

    Updating stuffs

---
+++ ios/build/Obhijog/Build/Intermediates.noindex/ReactNativeLocalization.build/Debug-iphonesimulator/ReactNativeLocalization.build/Objects-normal/x86_64/ReactLocalization.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/charchill/obhijog-app/node_modules/react-native-localization/ReactLocalization.m \
+  /Users/charchill/obhijog-app/node_modules/react-native-localization/ReactLocalization.h \
+  ../react-native/React/Base/RCTBridgeModule.h \
+  /Users/charchill/obhijog-app/ios/build/Obhijog/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  ../react-native/React/Base/RCTLog.h \
+  /Users/charchill/obhijog-app/ios/build/Obhijog/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h \
+  /Users/charchill/obhijog-app/ios/build/Obhijog/Build/Products/Debug-iphonesimulator/include/React/RCTUtils.h
